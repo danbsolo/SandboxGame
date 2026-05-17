70223750d08e6016ded48de5ec19576696bd63a9
Daniel <beteab.solomon@gmail.com>
2026-05-16 22:52:00 -0400

    change dimensions of screen

--- scripts/header.py
+++ scripts/header.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from random import randint, uniform, choice
 
-SCREEN_WIDTH = 1366 #640 #1920
-SCREEN_HEIGHT = 768 #480 #1080
+SCREEN_WIDTH = 1920 #1366 #640
+SCREEN_HEIGHT = 1080 #768 #480
 
 CONTAINER_DIVIDER = 2
